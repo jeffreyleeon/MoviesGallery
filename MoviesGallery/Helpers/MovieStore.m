d3a01998bd2d2cb4c2280581fca90e3ba6cbb73e
JeffreyLee <jeffreyleeon@gmail.com>
2016-01-31 10:51:30 +0800

    Fetch a basic api

--- MoviesGallery/Helpers/MovieStore.m
+++ MoviesGallery/Helpers/MovieStore.m
@@ -1,6 +1,9 @@
 #import "MovieStore.H"
+#import <ILMovieDBClient.h>
 
 @implementation MovieStore
+
+#define MOVIE_DB_API_KEY @"327b8d0c66c74e5eab2ebbec7b6ba4ce"
 
 #pragma mark Singleton Methods
 
@@ -15,6 +18,7 @@
 
 - (id)init {
     if (self = [super init]) {
+        [ILMovieDBClient sharedClient].apiKey = MOVIE_DB_API_KEY;
     }
     return self;
 }
@@ -22,8 +26,16 @@
 - (void)dealloc {
 }
 
-- (void) fetchTopMovies {
-    
+- (void)fetchTopMovies {
+    NSDictionary *params = @{
+        @"page": @"1",
+        @"language": @"en"
+    };
+    [[ILMovieDBClient sharedClient] GET:kILMovieDBMovieTopRated parameters:params block:^(id responseObject, NSError *error) {
+        if (!error) {
+            NSLog(@"%@", responseObject);
+        }
+    }];
 }
 
 @end
